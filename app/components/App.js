2229ca21950175f1428ec4a2f2277b3247576f28
Abdulkadir Abubakar <abdulkaadiras@gmail.com>
2024-05-11 14:06:52 +0100

    Added Responsiveness

--- app/components/App.js
+++ app/components/App.js
@@ -9,7 +9,7 @@ import Functions from "./Functions";
 
 const App = () => {
   return (
-    <div className="px-6 md:px-10 lg:px-14 xl:px-20">
+    <div className="px-6 md:px-10 lg:px-14 xl:px-20 2xl:w-[1620px] 2xl:my-0 2xl:mx-auto">
       <NavBar />
       <Hero />
       <Customers />
